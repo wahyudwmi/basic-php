5bc90eab6fb3b0a37c03d3ccb12f3491e45c37c7
Wahyu Dwi Amirullah <wahyudwmi@gmail.com>
2020-11-27 14:58:02 +0800

    Menambahkan file Komentar.php & Variable.php

---
+++ Komentar.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+komentar untuk banyak baris
+gunakan sintax ini.
+*/
+
+function textKomentar()
+{
+          //komentar untuk satu baris saja
+          echo 'Hello World';
+}
---
+++ Variable.php
@@ -0,0 +1,23 @@
+<?php
+
+$nama = 'Wahyu';
+$umur = '18';
+
+echo 'Nama : ';
+echo $nama;
+echo "\n";
+
+echo 'Umur : ';
+echo $umur;
+echo "\n";
+
+$contoh = "wahyu";
+$$contoh = "Dwi";
+
+echo 'Contoh : ';
+echo $contoh;
+echo "\n";
+
+echo 'Wahyu : ';
+echo $wahyu;
+echo "\n";
