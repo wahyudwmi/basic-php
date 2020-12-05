f8e9ce62d8b9b5e450e9100432cb90ad944f0ef3
Wahyu Dwi Amirullah <wahyudwmi@gmail.com>
2020-12-05 23:32:40 +0800

    Menambahkan file Break.php dan Constant.php

---
+++ Break.php
@@ -0,0 +1,12 @@
+<?php
+
+$nilai = 1;
+
+while (true) {
+          echo "ini adalah iterasi ke-$nilai" . PHP_EOL;
+          $nilai++;
+
+          if ($nilai > 10) {
+                    break;
+          }
+}
---
+++ Constant.php
@@ -0,0 +1,12 @@
+<?php
+
+define("Nama", "Wahyu Dwi Amirullah");
+define("Nilai", 90);
+
+echo "Nama : ";
+echo Nama;
+echo "\n";
+
+echo "Nilai : ";
+echo Nilai;
+echo "\n";
